e7e78e72d73d5e96509ab90bf64ce828339fe4ec
prems021 <premji.netcom@gmail.com>
2018-01-13 10:04:09 +0530

    Create tas_receipt.ts

---
+++ server/models/tas_receipt.ts
@@ -0,0 +1,44 @@
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Tas_receipts = sequelize.define('tas_products', {
+    ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+   
+      CUSTOMER_NAME: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+      PRODUCT_NAME: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    TAS_MASTER_ID: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+    },
+     TAS_SLAVE_ID: {
+    type: Sequelize.INTEGER,
+    allowNull: true
+    }
+   
+      PRICE: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    
+      GST_SLAB: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+  
+  }, {
+    tableName: 'tas_products',
+     timestamps: true
+  });
+
+
+export {Tas_products};
