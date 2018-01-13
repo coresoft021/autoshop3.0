a0f76f0a06f7c622478f79e04113478099f4250b
prems021 <premji.netcom@gmail.com>
2018-01-13 11:12:22 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
 import { Tas_products } from '../models/tas_product';
+import { Tas_receipts } from '../models/tas_receipts';
 import { Sequelize, sequelize } from './dbcon';
 const publicRouter: Router = Router();
 
