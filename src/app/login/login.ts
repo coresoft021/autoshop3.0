262dc3d4e9529eb824b950840dd8814b31da170a
prems021 <premji.netcom@gmail.com>
2018-02-03 13:31:21 +0530

    Update login.ts

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
-import { AuthenticationService } from '../services/login.service';
-import { User } from '../dash/usermodel';
+
 
 
 @Component({
@@ -12,36 +10,13 @@ import { User } from '../dash/usermodel';
 })
 export class LoginComponent implements OnInit {
  
-  myForm: FormGroup;
-  model = new User('','');
-  isclicked : boolean= false;
-  constructor( private router: Router,private fb: FormBuilder, private aS: AuthenticationService) {   }
+  
   ngOnInit()  {
-    this.isclicked=false;
-   this.myForm = this.fb.group({
-            username: [''],
-            password: ['']             
-                                      });
-
+   
   }
    
-    login() {
    
-  this.isclicked = true;
-    this.aS.login(this.myForm.value)
-    .subscribe(data => { 
-                    
-                     
-                      this.router.navigate(['/dash']);
-                     
-                },
-                
-                error => {
-                    this.router.navigate(['/']);
-                    
-                });
    
-        }
   }
 
 
