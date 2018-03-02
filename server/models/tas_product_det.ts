572512226f71814be2d253db114a447c4b2b00cc
prems021 <premji.netcom@gmail.com>
2018-03-02 13:26:21 +0530

    Update tas_product_det.ts

--- server/models/tas_product_det.ts
+++ server/models/tas_product_det.ts
@@ -1,1 +1,43 @@
-d
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Tas_product_det = sequelize.define('tas_product_det', {
+    ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    PRODUCT_NAME: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    CODE: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+      BATCH: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      HSN: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      DATEOFPUR: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      DATEOFEXP: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    
+   
+  
+  }, {
+    tableName: 'tas_product_det',
+     timestamps: true
+  });
+
+
+export {Tas_product_det};
