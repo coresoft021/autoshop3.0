c41298a529a90cce52511a296b98723517a383b4
prems021 <premji.netcom@gmail.com>
2018-03-08 21:58:14 +0530

    Update app.router.ts

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,20 +2,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { DashComponent } from './dash/dash';
 import { NgModule } from '@angular/core';
-import { Set1Component } from './test/set1';
+import { DashComponent } from  './dash/dash';
+import { TestExample } from './test/test';
+import { Stock_view } from './stock/view/stock_view';
+import { AddproductComponent } from './stock/add/add_stock_item_new';
+import { Stockentry } from './stock/entry/stock_entry';
 
-export const routes: Routes = [ 
-    { path:'', redirectTo: '/set1', pathMatch: 'full' },
-    { path: '', component: Set1Component},
-    { path: 'login', component: Set1Component },
+const routes: Routes = [
+    { path: '',component:DashComponent  },
+     { path: 'login',component: DashComponent },
+    { path: 'dash', component: DashComponent },
+   { path: 'reciept', component: TestExample },
+   {path: 'stock_view', component: Stock_view},
+   {path: 'add_new_product',component: AddproductComponent},
+   { path: 'stock_entry',component:Stockentry}, 
    
-   
-  
-      
 ];
+
 @NgModule({
- 
-  imports: [ RouterModule.forRoot(routes, { useHash : true }) ],
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
+
+
+
+
