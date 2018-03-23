56d02ca62180abb1a8713dc7d1570efb25c5f95c
prems021 <premji.netcom@gmail.com>
2018-03-23 15:31:11 +0530

    Update app.router.ts

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './login/login';
 import { DashComponent } from './dash/dash';
 import { NgModule } from '@angular/core';
 import { DashComponent } from  './dash/dash';
-import { TestExample } from './test/test';
 import { Stock_view } from './stock/view/stock_view';
 import { AddproductComponent } from './stock/add/add_stock_item_new';
 import { Stockentry } from './stock/entry/stock_entry';
@@ -12,7 +11,7 @@ const routes: Routes = [
     { path: '',component:DashComponent  },
      { path: 'login',component: DashComponent },
     { path: 'dash', component: DashComponent },
-   { path: 'reciept', component: TestExample },
+   { path: 'reciept', component: DashComponent },
    {path: 'stock_view', component: Stock_view},
    {path: 'add_new_product',component: AddproductComponent},
    { path: 'stock_entry',component:Stockentry}, 
