273765ae220608cd266d8694155bfdbad0bcf061
prems021 <premji.netcom@gmail.com>
2018-04-11 22:00:01 +0530

    Update tas_master.ts

--- server/models/tas_master.ts
+++ server/models/tas_master.ts
@@ -1,1 +1,54 @@
-k
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Tas_master = sequelize.define('tas_master', {
+    ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    INVOICE_NUMBER: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    CUSTOMER_NAME: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+      CUSTOMER_ADDRESS: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+     CUSTOMER_PHONE: {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    },
+      DOCTOR_NAME: {
+      type: Sequelize.STRING,
+      allowNull: true
+    },
+       DOCTOR_PHONE: {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    },
+      DISCOUNT_TOTAL: {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: true
+    },
+    
+    TAX_COLLECTED: {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false
+    },
+     GROSS_TOTAL: {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false
+    },
+  
+  }, {
+    tableName: 'tas_master',
+     timestamps: true
+  });
+
+
+export {Tas_master};
