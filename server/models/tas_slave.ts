c81e8663358743b03d332e01c44819a7e8d89419
prems021 <premji.netcom@gmail.com>
2018-04-11 22:07:11 +0530

    Update tas_slave.ts

--- server/models/tas_slave.ts
+++ server/models/tas_slave.ts
@@ -1,1 +1,39 @@
-k
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Tas_slave = sequelize.define('tas_slave', {
+    ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },    
+      PRODUCT_NAME: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    QUANTITY: {
+       type: Sequelize.INTEGER,
+    allowNull: false
+    },
+      
+    TAS_MASTER_ID: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+    },
+      PRICE: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    
+      DISCOUNT: {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    },
+  
+  }, {
+    tableName: 'tas_slave',
+     timestamps: true
+  });
+
+
+export {Tas_slave};
