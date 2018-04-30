b57283b40a10438c75b0b83c5149d6b15035607f
prems021 <premji.netcom@gmail.com>
2018-04-30 14:06:59 +0530

    Update app.router.ts

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,15 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { NgModule } from '@angular/core';
-import { DashComponent } from  './dash/dash';
+
 
 
 
 const routes: Routes = [
-    { path: '',component:DashComponent  },
-     { path: 'login',component: DashComponent },
-    { path: 'dash', component: DashComponent },
-   { path: 'reciept', component: DashComponent },
+    { path: '',component: LoginComponent },
+     { path: 'login',component: LoginComponent },
+    
+  
 
 
    
