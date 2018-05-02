3d8b0665188608f69168133afd67f665f3937801
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-05-02 14:48:43 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -60,11 +60,13 @@ publicRouter.post('/add_user', (request: Request, response: Response) => {
 
 publicRouter.post('/add_new_product', (request: Request, response: Response) => {
  Tas_products.create({
-    PRODUCT_NAME: request.body.Productname,
-    CODE: request.body.Code,
-    PRICE: request.body.Price,
-    GST_SLAB: request.body.Gst_slab
-  
+    PRODUCT_NAME: request.body.product_name,
+    PRODUCT_CODE: request.body.product_code,
+    NET_PRICE: request.body.net_price,
+    TAX: request.body.tax,
+    AVAIL_QTY : request.body.avail_qty,
+    NET_PURCHASE_PRICE :request.body.net_purchase_price,
+    UNIT : request.body.unit
      })
      
   return response.json({success:true, msg:'Successfully saved'});
