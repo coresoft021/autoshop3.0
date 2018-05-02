98c16338ccf1c94d8696b0352b8d11098e4cf9d1
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-05-02 15:52:50 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -35,6 +35,45 @@ then(users => {
 });
 
 
+publicRouter.post('/add_new_product', (request: Request, response: Response) => {
+  
+  
+Tas_products.findOne({ where: { PRODUCT_NAME:  request.body.product_name } }).then(person => {
+
+  if(person){
+  
+                  return response.json({success:true, msg:'product already existed'});
+            }
+  
+  else 
+        {
+            Tas_products.findOne({ where: { PRODUCT_CODE:  request.body.product_code } }).then(pers => {
+
+                if(pers){
+                          return response.json({success:true, msg:'Code already existed'});
+                        }
+                           else{
+                                    Tas_products.create({
+                                        PRODUCT_NAME: request.body.product_name,
+                                        PRODUCT_CODE: request.body.product_code,
+                                        NET_PRICE: request.body.net_price,
+                                        TAX: request.body.tax,
+                                        AVAIL_QTY : request.body.avail_qty,
+                                        NET_PURCHASE_PRICE :request.body.net_purchase_price,
+                                        UNIT : request.body.unit
+                                        })
+     
+                                      return response.json({success:true, msg:'Successfully saved'});
+    
+                                 }
+            }) 
+
+        }
+  
+    })
+
+})
+
 
 
 
@@ -58,40 +97,7 @@ publicRouter.post('/add_user', (request: Request, response: Response) => {
      
  });
 
-publicRouter.post('/add_new_product', (request: Request, response: Response) => {
-  
-  
-Tas_products.findOne({ where: { PRODUCT_NAME:  request.body.product_name } }).then(person => {
 
-  if(person){
-  
-   return response.json({success:true, msg:'product already existed'});
-  }
-  
-  else 
-  {
-    Tas_products.findOne({ where: { PRODUCT_CODE:  request.body.product_code } }).then(pers => {
-
-  if(pers){
-     return response.json({success:true, msg:'Code already existed'});
-         }
-      else{
- Tas_products.create({
-    PRODUCT_NAME: request.body.product_name,
-    PRODUCT_CODE: request.body.product_code,
-    NET_PRICE: request.body.net_price,
-    TAX: request.body.tax,
-    AVAIL_QTY : request.body.avail_qty,
-    NET_PURCHASE_PRICE :request.body.net_purchase_price,
-    UNIT : request.body.unit
-     })
-     
-  return response.json({success:true, msg:'Successfully saved'});
-    
-  }
-  
-     
- });
 
 
 
