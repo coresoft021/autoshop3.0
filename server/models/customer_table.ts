b5fb8c946ccc52ae8225a36c6cb7f5e884ce9536
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-05-15 11:20:21 +0530

    Update customer_table.ts

--- server/models/customer_table.ts
+++ server/models/customer_table.ts
@@ -1,1 +1,30 @@
-l
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Tas_customers = sequelize.define('tas_customers', {
+    ID: {
+      type: Sequelize.INTEGER(15),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    CUSTOMER_NAME: {
+      type: Sequelize.STRING(15),
+      allowNull: false
+    },
+    STREET: {
+      type: Sequelize.STRING(15),
+      allowNull: true
+    },
+    
+    TIN: {
+      type: Sequelize.STRING(15),
+      allowNull: true
+    },
+  
+  }, {
+    tableName: 'tas_customers',
+     timestamps: false
+  });
+
+
+export {Tas_customers};
