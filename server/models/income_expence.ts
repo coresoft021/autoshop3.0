fe9f36ebeb2884cb3513fbdc9f4ee1b4d56f614a
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-05-15 11:53:54 +0530

    Update income_expence.ts

--- server/models/income_expence.ts
+++ server/models/income_expence.ts
@@ -1,1 +1,61 @@
-s
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Tas_income_expence = sequelize.define('tas_income_expence', {
+    ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    INVOICE_NUMBER: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      ESTIMATE_NUMBER: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      IS_INVOICE: {
+      type: Sequelize.BOOLEAN,
+      allowNull: true
+    },
+    
+     TRAN_TYPE: {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    },
+     
+      SUB_EXPENCE_CATEGORY: {
+      type: Sequelize.STRING(15),
+      allowNull: false
+    },
+    
+    TAX_COLLECTED: {
+      type: Sequelize.INTEGER(20),
+      allowNull: false
+    },
+     TOTAL_AMOUNT: {
+      type: Sequelize.INTEGER(25),
+      allowNull: false
+    },
+     
+    TOTAL_PAYED: {
+      type: Sequelize.INTEGER(25),
+      allowNull: true
+    },
+    TOTAL_DUE: {
+      type: Sequelize.INTEGER(25),
+      allowNull: true
+    },
+     IS_PARTIAL_PAY: {
+      type: Sequelize.BOOLEAN,
+      allowNull: true
+    },
+  
+  }, {
+    tableName: 'tas_income_expence',
+     timestamps: true
+  });
+
+
+export {Tas_income_expence};
