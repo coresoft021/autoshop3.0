ac4c4f4c09b1fb68acd58a0efffa9c602ed5cf3b
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-05-19 21:25:09 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -20,7 +20,7 @@ const Op = Sequelize.Op;
    if (option === 2)
    {
       Tas_income_expence.findAll({
-        where: { createdAt: {[Op.between]:  [request.body.from_date ,  request.body.to_date ]}, TRAN_TYPE : 3}
+        where: { createdAt: {[Op.between]:  [request.body.from_date ,  request.body.to_date ]}, TRAN_TYPE : 1}
                                 })
          .then(users=> 
               {
@@ -41,7 +41,7 @@ const Op = Sequelize.Op;
    else if (option === 3) {
      
                     Tas_income_expence.findAll({
-                    where: { createdAt: {[Op.between]:  [request.body.from_date ,  request.body.to_date ]}, TRAN_TYPE : 1}
+                    where: { createdAt: {[Op.between]:  [request.body.from_date ,  request.body.to_date ]}, TRAN_TYPE : 3}
                                 })
                                .then(users=> 
                                               {
