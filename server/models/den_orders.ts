4d8d03ca3c63d99477d4434823092872e2d3eb96
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-06-16 10:06:23 +0530

    Update and rename tas_product.ts to den_orders.ts

---
+++ server/models/den_orders.ts
@@ -0,0 +1,43 @@
+import {  sequelize , Sequelize } from '../routes/dbcon';
+
+  const Den_orders = sequelize.define('den_orders', {
+    ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    ORDER_NUMBER: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    CLIENT_ID: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      PATIANT_NAME: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    
+    SIDE: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    NUMBER: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+      MATERIAL: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+     
+  
+  }, {
+    tableName: 'den_orders',
+     timestamps: false
+  });
+
+
+export {Den_orders};
