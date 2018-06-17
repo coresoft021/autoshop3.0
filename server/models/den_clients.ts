0f2e4571d8a294607d218b0dd51ff704caad5d26
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-06-17 15:19:08 +0530

    Update den_clients.ts

--- server/models/den_clients.ts
+++ server/models/den_clients.ts
@@ -2,32 +2,32 @@ import {  sequelize , Sequelize } from '../routes/dbcon';
 
   const Den_clients = sequelize.define('den_clients', {
     ID: {
-      type: Sequelize.INTEGER(15),
+      type: Sequelize.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     CUSTOMER_NAME: {
-      type: Sequelize.STRING(15),
+      type: Sequelize.STRING(25),
       allowNull: false
     },
     STREET: {
-      type: Sequelize.STRING(15),
+      type: Sequelize.STRING(25),
       allowNull: true
     },
      CITY: {
-      type: Sequelize.STRING(15),
+      type: Sequelize.STRING(25),
       allowNull: true
     },
     
       
      PHONE_NUMBER: {
-      type: Sequelize.INTEGER(12),
+      type: Sequelize.INTEGER(13),
       allowNull: true
     },
     
        E_MAIL: {
-      type: Sequelize.STRING(15),
+      type: Sequelize.STRING(35),
       allowNull: true
     },
   
