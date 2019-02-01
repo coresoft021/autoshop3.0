71d309a46c6fe96bd11d2ec7e85c2dc99bab8314
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-01 21:41:33 +0530

    Update cdb.ts

--- server/routes/cdb.ts
+++ server/routes/cdb.ts
@@ -8,6 +8,25 @@ import { Sequelize, sequelize } from './dbcon';
 
 const dbRouter: Router = Router();
 
+dbRouter.get("/all_1", (request: Request, response: Response) => {
+
+
+     Tas_users.sync({force: true}).then(() => {
+   //Table created
+  return Tas_users.create({
+    
+     USER_NAME : 'a',
+     PASSWORD: 's',
+     IS_ADMIN: true,
+    
+      
+    
+     })
+     })
+
+
+ return response.json({success:true, msg: 'found'});
+  })
 
 
 dbRouter.get("/about", (request: Request, response: Response) => {
