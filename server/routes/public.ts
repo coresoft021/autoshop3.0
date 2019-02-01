93875030b175e3c6b218c9223af5f4e55b2b0125
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-01 19:21:46 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -1,105 +1,10 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
-import { Den_clients } from '../models/den_clients';
-import { Den_orders } from '../models/den_orders';
 import { Sequelize, sequelize } from './dbcon';
 const publicRouter: Router = Router();
 const Op = Sequelize.Op;
 
 
-
-publicRouter.get('/get_all_orders', (request: Request, response: Response) => {
- 
-   
-  Den_orders.findAll({
-  
- }).
-then(users => {
-  response.send(users);
-  });
-
-});
-
-publicRouter.get('/get_clients', (request: Request, response: Response) => {
- 
-   
-  Den_clients.findAll({
-  
- }).
-then(users => {
-  response.send(users);
-  });
-
-});
-
-  publicRouter.post('/get_order_number', (request: Request, response: Response) => {
-    Den_orders.count().then(c => {
-           response.json({
-                                  text: "counted",
-                                  count: c,
-                      });
-              
-            })
-   
-   })
-
-
-publicRouter.post('/post_new_order', (request: Request, response: Response) => {
-
-    Den_orders.create({             
-                                        ORDER_NUMBER : request.body.order_number,
-                                        CLIENT_ID : request.body.client_id,
-                                        PATIANT_NAME      : request.body.patiant_name,
-                                        SIDE    :request.body.selectedSide,
-                                        NUMBER    :request.body.selectedNumber,
-                                        MATERIAL    :request.body.selectedMaterial,
-                                        STATUS    :request.body.selectedStatus,
-                                        })
-  
-          
-       
-    return response.json({success:true, msg:'Successfully saved'});
-    
-         
- })
-
-
-publicRouter.post('/add_new_client', (request: Request, response: Response) => {
-
-    Den_clients.findOne({ where: { CUSTOMER_NAME:  request.body.customer_name } }).then(person => {
-
-  if(person){
-  
-                  return response.json({success:true, msg:'Client name already existed'});
-            }
-  else    {
-    
-                Den_clients.create({             
-                                        CUSTOMER_NAME : request.body.customer_name,
-                                        STREET : request.body.street,
-                                        CITY      : request.body.city,
-                                        PHONE_NUMBER   : request.body.phone_number,
-                                        E_MAIL    :request.body.email,
-                                       
-                                       
-                                        })
-  
-
-      
-
-   
-    
-  
-           }
-       
-    return response.json({success:true, msg:'Successfully saved'});
-    
-         
- })
-    });
-
-                          
- 
 
 
 export { publicRouter };
