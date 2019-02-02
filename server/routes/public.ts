a157bbc6b4c1a2054b74181b0b60d5af849b9307
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-02 15:36:49 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -5,6 +5,19 @@ import { Sequelize, sequelize } from './dbcon';
 const publicRouter: Router = Router();
 const Op = Sequelize.Op;
 
+
+ publicRouter.get('/get_product_list', (request: Request, response: Response) => {
+ 
+   
+  Tas_products.findAll({
+  
+ }).
+then(users => {
+  response.send(users);
+  });
+
+});
+
 publicRouter.get('/get_customer_list', (request: Request, response: Response) => {
  
    
