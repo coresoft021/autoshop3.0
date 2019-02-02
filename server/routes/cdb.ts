50f5ad0d5268139e64d0dd398e880f96e0c9ac5e
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-02 14:44:13 +0530

    Update cdb.ts

--- server/routes/cdb.ts
+++ server/routes/cdb.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
+import { Tas_customers } from '../models/tas_customers';
+
 
 
 import { Sequelize, sequelize } from './dbcon';
@@ -11,14 +13,12 @@ const dbRouter: Router = Router();
 dbRouter.get("/all_1", (request: Request, response: Response) => {
 
 
-     Tas_users.sync({force: true}).then(() => {
+     Tas_customers.sync({force: true}).then(() => {
    //Table created
-  return Tas_users.create({
+  return Tas_customers.create({
     
-     USER_NAME : 'a',
-     PASSWORD: 's',
-     IS_ADMIN: true,
-    
+     CUSTOMER_NAME : 'a',
+     
       
     
      })
