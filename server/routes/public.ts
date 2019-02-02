98fb597431ef6f23c23a59cc6aa5a8d2deb66c61
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-02 15:08:54 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -1,8 +1,21 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
+import { Tas_customers } from '../models/tas_customers';
 import { Sequelize, sequelize } from './dbcon';
 const publicRouter: Router = Router();
 const Op = Sequelize.Op;
+
+publicRouter.get('/get_customer_list', (request: Request, response: Response) => {
+ 
+   
+  Tas_customers.findAll({
+  
+ }).
+then(users => {
+  response.send(users);
+  });
+
+});
 
 
 
