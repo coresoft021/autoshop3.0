cb38b636bc3fceb149302d22f0cabcc8c4ddc796
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-05 12:35:23 +0530

    Update post.ts

--- server/routes/post.ts
+++ server/routes/post.ts
@@ -1,5 +1,12 @@
 
-
+import { Request, Response, Router } from "express";
+import { Tas_users } from '../models/tas_users';
+import { Tas_customers } from '../models/tas_customers';
+import { Tas_products } from '../models/tas_products';
+import { B2b_invoice_master } from '../models/b2b_master';
+import { Sequelize, sequelize } from './dbcon';
+const publicRouter: Router = Router();
+const Op = Sequelize.Op;
 
 
 
