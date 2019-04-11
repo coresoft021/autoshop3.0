aff463f2407f4c4ed20e01a0af6aec7fd3226d75
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 18:16:58 +0530

    Update cdb.ts

--- server/routes/cdb.ts
+++ server/routes/cdb.ts
@@ -1,21 +1,25 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
-import { Tas_customers } from '../models/tas_customers';
+import { Tas_feed_master  } from '../models/tas_feed_master';
 import { Sequelize, sequelize } from './dbcon';
 const dbRouter: Router = Router();
 
-dbRouter.get("/all", (request: Request, response: Response) => {
-
-
-     Tas_products.sync({force: true}).then(() => {
+dbRouter.get("/feed_master", (request: Request, response: Response) => {
+     Tas_feed_master.sync({force: true}).then(() => {
    //Table created
-  return Tas_products.create({    
+  return Tas_feed_master.create({    
      FEED_BACK_ID : 1,   
      })
      })
-
-
-
+ return response.json({success:true, msg: 'found'});
+  })
+dbRouter.get("/user", (request: Request, response: Response) => {
+     Tas_users.sync({force: true}).then(() => {
+   //Table created
+  return Tas_users.create({    
+     USER_NAME : 1,   
+     })
+     })
  return response.json({success:true, msg: 'found'});
   })
 
