3f4d3cc9874f0aa9790e21d124fd207ce9166f18
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 18:14:46 +0530

    Update cdb.ts

--- server/routes/cdb.ts
+++ server/routes/cdb.ts
@@ -1,14 +1,7 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
 import { Tas_customers } from '../models/tas_customers';
-
-
-
-
 import { Sequelize, sequelize } from './dbcon';
-
- 
-
 const dbRouter: Router = Router();
 
 dbRouter.get("/all", (request: Request, response: Response) => {
@@ -27,25 +20,7 @@ dbRouter.get("/all", (request: Request, response: Response) => {
   })
 
 
-dbRouter.get("/about", (request: Request, response: Response) => {
-  
-return response.json({success:true, msg: 'found'});
-   })
 
-
-dbRouter.get("/user_table", (request: Request, response: Response) => {
-  
-  
-
-
-dbRouter.get("/slave", (request: Request, response: Response) => {
-  
-  
-       B2b_invo_slave.sync({force: true}).then(() => {
-   //Table created
-  return B2b_invo_slave.create({
-    
-     B2B_MASTER_ID : 1,
 
 
 export { dbRouter };
